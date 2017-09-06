d28958c17d3e5f4da980331707bd2c87f9b2195c
K9274 <K9274@student.jamk.fi>
2017-09-06 09:03:03 +0300

    lähen raapimaan olutta. muokkasin myös jotain tiedostoja joita en myönnä muokanneeni. tilanne on nyt se että mitään en voi vahvistaa, enkä kieltää, vaan pikemminkin päinvastoin.

--- OOdemot/Program.cs
+++ OOdemot/Program.cs
@@ -1,7 +1,10 @@
-﻿using JAMK.IT; //helpottaa meidän tekemien luokkien löytämistä
+﻿//TTOS0200 Olio-ohjelmointi viikkotehtävät
+// Adolf Hitler
+
+using JAMK.IT; //helpottaa meidän tekemien luokkien löytämistä
 using System;
 
-namespace OOdemot
+namespace JAMK.IT
 {
   class Program
   {
